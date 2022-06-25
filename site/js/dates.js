e1d9b51ff8f4fc5e7f7e451491f46dbbcda19a6c
Nathan-Weber <nathanwbr@gmail.com>
2022-06-25 10:51:17 -0600

    commit

---
+++ site/js/dates.js
@@ -0,0 +1,33 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+let d = new Date(document.lastModified);
+console.log(d)
+let dayname = daynames[d.getDay()];
+let monthname = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayname + ", " + monthname + " " + d.getDate() + ", " + year;
+
+
+document.getElementById("currentdate").innerHTML = fulldate;
+document.getElementById("currentyear").innerHTML = year;
