78e88caae4bfa11d7830936eef1fa961f21d3725
Nathan-Weber <nathanwbr@gmail.com>
2022-07-05 14:00:29 -0600

    COMMIT

---
+++ rate_my_course/scripts/Jsonmanip.js
@@ -0,0 +1,3 @@
+const data = require('C:/Users/Natha/OneDrive/Desktop/applied prog/RateMy/rate_my_course/rateMyClassData.json');   // data to be loaded
+
+console.log(data.university.map(university => university.name));
